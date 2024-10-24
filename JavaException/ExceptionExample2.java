6e671353751936a8a261c9480e9853f79980df0b
Kshitij Nikose <nikokshitij9970@gmail.com>
2024-10-24 23:47:14 +0530

    Add files via upload

---
+++ JavaException/ExceptionExample2.java
@@ -0,0 +1,12 @@
+public class ExceptionExample2 {
+    public static void main(String[] args) {
+        System.out.println("Reset of the code....");
+
+        try
+        {
+            System.out.println("in try block");
+            int a=10/0;
+        }//try end
+    }
+    
+}
---
+++ JavaException/TryCatchExample1.java
@@ -0,0 +1,9 @@
+public class TryCatchExample1 {
+
+    public static void main(String[] args) {
+        
+        int data=50/0;
+
+    }
+    
+}

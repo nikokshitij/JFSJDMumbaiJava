72e1c7e28d253a430e1e1e93d8e0465edf84ea12
Kshitij Nikose <nikokshitij9970@gmail.com>
2024-10-24 23:34:18 +0530

    Add files via upload

---
+++ Basicjava/src/dataType.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class dataType {
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        String name="Kshitij";
+        String fname="Kshitij";
+
+        long c=15;
+        long d=10;
+
+        float a1=22;
+        float b1=21;
+
+        Short a2=0;
+        Short b2=0;
+
+        byte a3=0;
+        byte b3=0;
+
+        if(a==b){
+            System.out.println("int type");
+        }
+        if (name.equals(fname)) {
+            System.out.println("string type");   
+        }
+        if (c==d){
+            System.out.println("long type");    
+        }
+        if (a1==b1) {
+            System.out.println("float type");    
+        }
+        if (a2==b2) {
+            System.out.println("short type");       
+        }
+        if (a3==b3) {
+            System.out.println("byte type");   
+            
+        }
+        
+    }   
+}
